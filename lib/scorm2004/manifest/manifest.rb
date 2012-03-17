7bd6a846b6ac1b7045801ef651ae7ff09fba8704
Takahiro Noda <takahiro.noda@gmail.com>
2012-03-17 15:48:44 +0900

    manifest/manifest.rb: introduce VERSION_SPM

--- lib/scorm2004/manifest/manifest.rb
+++ lib/scorm2004/manifest/manifest.rb
@@ -6,6 +6,8 @@ module Scorm2004
       include Attributes
       include XmlBase
       
+      VERSION_SPM = 20
+
       has_one_and_only_one './imscp:metadata/imscp:schema'
       has_one_and_only_one './imscp:metadata/imscp:schemaversion'
       has_one_and_only_one 'imscp:resources'
@@ -13,7 +15,7 @@ module Scorm2004
       has_zero_or_one      'imsss:sequencingCollection'
 
       attribute :id,     'identifier'
-      attribute :string, 'version', spm: 20
+      attribute :string, 'version', spm: VERSION_SPM
     end
   end
 end
